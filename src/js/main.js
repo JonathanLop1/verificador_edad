0f9a76f813c9d7ba9293d85327940f6bccc1435b
JonathanLop1 <147114171+JonathanLop1@users.noreply.github.com>
2025-06-20 18:11:31 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,26 @@ function calc() {
     let ageUser = document.getElementById("age").value
     let resultUser = document.getElementById("rest")
     let triedUser = document.getElementById("intent")
+    let vacio = nameUser.trim();
     //We validate whether the person is a minor, an adult, or a negative number.
-    if (ageUser < 18 && ageUser > 0) {
-        //We use internalhtml to print in html documents.
-        resultUser.innerHTML = `Hola ${nameUser}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`
-        let calculation = 18 - ageUser
-        triedUser.innerHTML = `Hola ${nameUser}, te falta ${calculation} año para poder ser mayor de edad`
+    if(/\d/.test(nameUser)==false){
+        if (ageUser < 18 && ageUser > 0) {
+            //We use internalhtml to print in html documents.
+            resultUser.innerHTML = `Hola ${nameUser}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`
+            let calculation = 18 - ageUser
+            triedUser.innerHTML = `Hola ${nameUser}, te falta ${calculation} año para poder ser mayor de edad`
+        }
+    
+        if (ageUser < 0) {
+            resultUser.innerHTML = `Hola ${nameUser}, estas ingresando un numero negativo, por favor ingresa correctamente este dato`
+        }
+    
+        if (ageUser >= 18) {
+            triedUser.innerHTML = ""
+            resultUser.innerHTML = `Hola ${nameUser}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`
+        }
+    }else{
+        alert("ERROR")
     }
-
-    if (ageUser < 0) {
-        resultUser.innerHTML = `Hola ${nameUser}, estas ingresando un numero negativo, por favor ingresa correctamente este dato`
-    }
-
-    if (ageUser >= 18) {
-        triedUser.innerHTML = ""
-        resultUser.innerHTML = `Hola ${nameUser}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`
-    }
+    
 }
